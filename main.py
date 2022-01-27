ebf3f4cc5079596e3e7ddf78225afa0f92be0b9a
Sam Reyes <zohrlaffz@gmail.com>
2022-01-26 18:35:19 -0600

    Successful marking and blending of images. Missing GUI.

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 from tkinter import *
 from PIL import Image
 
-im = Image.open("mynft.jpg")
+wtrmrk = Image.open("watermark.png")
+im = Image.open("mynft.png")
 
 # print(im.format, im.size, im.mode)
+# im.show()
 
-im.show()
+# Use this to select entire image 'uploaded' by user (add watermark on top later)
+entire_image = (0, 0, im.size[0], im.size[1])
+
+# Center of image
+xsize, ysize = im.size
+
+xcenter, ycenter = (xsize // 2 - 100), (ysize // 2)
+top_left = (xsize // 4 - 100, ysize // 4)
+top_right = (round(xsize * .75 - 100), ysize // 4)
+bottom_left = (xsize // 4 - 100, round(ysize * .75))
+bottom_right = (round(xsize * .75 - 100), round(ysize * .75))
+
+
+canvas = Image.new("RGBA", im.size, (255, 255, 255, 0))
+watermark_layer = Image.new("RGBA", im.size, (255, 255, 255, 0))
+
+canvas.paste(im=im)
+
+watermark_layer.paste(im=wtrmrk, box=(xcenter, ycenter), mask=wtrmrk)
+watermark_layer.paste(im=wtrmrk, box=top_left, mask=wtrmrk)
+watermark_layer.paste(im=wtrmrk, box=top_right, mask=wtrmrk)
+watermark_layer.paste(im=wtrmrk, box=bottom_left, mask=wtrmrk)
+watermark_layer.paste(im=wtrmrk, box=bottom_right, mask=wtrmrk)
+
+image_blend = Image.blend(canvas, watermark_layer, .15)
+image_blend.show()
+
+# canvas.save("watermarked_image.png")
+

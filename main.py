5b28dc84bfa68491f146e53cf7b8a6a398f643f8
Sam Reyes <zohrlaffz@gmail.com>
2022-01-30 14:11:41 -0600

    Cotinue Logic and GUI drafts

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
+from tkinter import *
+from tkinter.filedialog import askopenfile, asksaveasfile
 from PIL import Image
 
+
+def open_file():
+    file = askopenfile(mode='r', filetypes=[('Image Files', '*.png'), ('All Files', '*.*')])
+    file.write("user_image.png")
+
+
+def save_file():
+    return asksaveasfile(mode='w', confirmoverwrite=True, defaultextension='png', initialfile="watermarked_image.png")
+
+
+window = Tk()
+window.title("Watermarker")
+window.config()
+
+# Logo section
+canvas = Canvas(width=500, height=100, bg="white")
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(250, 50, image=logo_img)
+canvas.pack()
+
+# Load image section
+load_title = Label(text="Choose Image")
+load_title.pack()
+load_button = Button(text="Open", command=open_file)
+load_button.pack()
+
+# Save image section
+save_title = Label(text="Save")
+save_title.pack()
+save_button = Button(text="Save", command=save_file)
+save_button.pack()
+
 wtrmrk = Image.open("watermark.png")
-im = Image.open(open_file())
+im = Image.open("user_image.png")
 
 # Use this to select entire image 'uploaded' by user (add watermark on top later)
 entire_image = (0, 0, im.size[0], im.size[1])
@@ -14,7 +48,6 @@ top_left = (xsize // 4 - 100, ysize // 4)
 top_right = (round(xsize * .75 - 100), ysize // 4)
 bottom_left = (xsize // 4 - 100, round(ysize * .75))
 bottom_right = (round(xsize * .75 - 100), round(ysize * .75))
-
 
 canvas = Image.new("RGBA", im.size, (255, 255, 255, 0))
 watermark_layer = Image.new("RGBA", im.size, (255, 255, 255, 0))
@@ -32,4 +65,7 @@ image_blend.show()
 
 image_blend.save("watermarked_image.png")
 
-# TODO 1. GUI
+window.mainloop()
+
+
+# TODO 1. Figure out how to load images (save in directory), process image with watermark, save edited images after

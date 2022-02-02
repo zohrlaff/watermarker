a6b7c3f8b4a4af7bc8631595430715b5781a4fc4
Sam Reyes <zohrlaffz@gmail.com>
2022-02-01 20:54:23 -0600

    Image watermarking function complete.

--- main.py
+++ main.py
@@ -1,15 +1,56 @@
 from tkinter import *
-from tkinter.filedialog import askopenfile, asksaveasfile
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 from PIL import Image
 
+# TODO 1. Document, Refactor, Tweak
 
 def open_file():
-    file = askopenfile(mode='r', filetypes=[('Image Files', '*.png'), ('All Files', '*.*')])
-    file.write("user_image.png")
+    global image_blend
+
+    # Capture file path
+    file_name = askopenfilename(title="Select an image", initialdir="C:\\",
+                                filetypes=[("All Files", "*.*"), ("Image Files", "*.png")])
+
+    wtrmrk = Image.open("watermark.png")
+    im = Image.open(file_name)
+
+    # Use this to select entire image 'uploaded' by user (add watermark on top later)
+    entire_image = (0, 0, im.size[0], im.size[1])
+
+    # Center of image
+    xsize, ysize = im.size
+
+    xcenter, ycenter = (xsize // 2 - 100), (ysize // 2)
+    top_left = (xsize // 4 - 100, ysize // 4)
+    top_right = (round(xsize * .75 - 100), ysize // 4)
+    bottom_left = (xsize // 4 - 100, round(ysize * .75))
+    bottom_right = (round(xsize * .75 - 100), round(ysize * .75))
+
+    canvas = Image.new("RGBA", im.size, (255, 255, 255, 0))
+    watermark_layer = Image.new("RGBA", im.size, (255, 255, 255, 0))
+
+    canvas.paste(im=im)
+
+    watermark_layer.paste(im=wtrmrk, box=(xcenter, ycenter), mask=wtrmrk)
+    watermark_layer.paste(im=wtrmrk, box=top_left, mask=wtrmrk)
+    watermark_layer.paste(im=wtrmrk, box=top_right, mask=wtrmrk)
+    watermark_layer.paste(im=wtrmrk, box=bottom_left, mask=wtrmrk)
+    watermark_layer.paste(im=wtrmrk, box=bottom_right, mask=wtrmrk)
+
+    image_blend = Image.blend(canvas, watermark_layer, .2)
+    # image_blend.show()
+
+    image_blend.save("watermarked_image.png")
 
 
 def save_file():
-    return asksaveasfile(mode='w', confirmoverwrite=True, defaultextension='png', initialfile="watermarked_image.png")
+    watermarked_image = asksaveasfilename(title="Save watermarked image", confirmoverwrite=True,
+                                          defaultextension=".png",
+                                          initialfile="watermarked_image.png", initialdir="C:\\",
+                                          filetypes=[("PNG File", "*.png"), ("JPG File", "*.jpg"),
+                                                     ("BMP File", "*.bmp"),
+                                                     ("All Files", "*.*")])
+    image_blend.save(f"{watermarked_image}")
 
 
 window = Tk()
@@ -34,38 +75,4 @@ save_title.pack()
 save_button = Button(text="Save", command=save_file)
 save_button.pack()
 
-wtrmrk = Image.open("watermark.png")
-im = Image.open("user_image.png")
-
-# Use this to select entire image 'uploaded' by user (add watermark on top later)
-entire_image = (0, 0, im.size[0], im.size[1])
-
-# Center of image
-xsize, ysize = im.size
-
-xcenter, ycenter = (xsize // 2 - 100), (ysize // 2)
-top_left = (xsize // 4 - 100, ysize // 4)
-top_right = (round(xsize * .75 - 100), ysize // 4)
-bottom_left = (xsize // 4 - 100, round(ysize * .75))
-bottom_right = (round(xsize * .75 - 100), round(ysize * .75))
-
-canvas = Image.new("RGBA", im.size, (255, 255, 255, 0))
-watermark_layer = Image.new("RGBA", im.size, (255, 255, 255, 0))
-
-canvas.paste(im=im)
-
-watermark_layer.paste(im=wtrmrk, box=(xcenter, ycenter), mask=wtrmrk)
-watermark_layer.paste(im=wtrmrk, box=top_left, mask=wtrmrk)
-watermark_layer.paste(im=wtrmrk, box=top_right, mask=wtrmrk)
-watermark_layer.paste(im=wtrmrk, box=bottom_left, mask=wtrmrk)
-watermark_layer.paste(im=wtrmrk, box=bottom_right, mask=wtrmrk)
-
-image_blend = Image.blend(canvas, watermark_layer, .15)
-image_blend.show()
-
-image_blend.save("watermarked_image.png")
-
 window.mainloop()
-
-
-# TODO 1. Figure out how to load images (save in directory), process image with watermark, save edited images after
